576a3c175dcd2a400bda7c1327d3a9e3ba38f3ae
9QS <micraftur@gmail.com>
2022-11-30 18:57:36 +0300

    Add ALLOW_MISSING_DEPENDENCIES=true, fix services.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,9 +1,9 @@
 DEVICE_PATH := device/samsung/j4lte
-
+ALLOW_MISSING_DEPENDENCIES := true
 # Partitions
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2625634304
 # Assert
-TARGET_OTA_ASSERT_DEVICE := j4lte, j4ltexx,j4ltejx,j4lteub
+TARGET_OTA_ASSERT_DEVICE := j4lte,j4ltexx,j4ltejx,j4lteub
 # Inherit common board flags
 include device/samsung/universal7570-common/BoardConfigCommon.mk
 include vendor/samsung/j4lte/BoardConfigVendor.mk
