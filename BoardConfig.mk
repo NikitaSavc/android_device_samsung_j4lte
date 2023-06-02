c9b112f861b0a6d62cc6818456aa658f1063dfbd
9QS <micraftur@gmail.com>
2023-06-02 17:29:13 +0300

    Changes

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,3 +7,13 @@ TARGET_OTA_ASSERT_DEVICE := j4lte,j4ltexx,j4ltejx,j4lteub
 # Inherit common board flags
 include device/samsung/universal7570-common/BoardConfigCommon.mk
 include vendor/samsung/j4lte/BoardConfigVendor.mk
+
+# Kernel config
+TARGET_KERNEL_CONFIG := exynos7570-j4lte_defconfig
+
+# Display
+TARGET_SCREEN_WIDTH := 720
+TARGET_SCREEN_HEIGHT := 1280
+
+# HIDL
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/manifest.xml
